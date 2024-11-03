c440f5a10a70bc3ef6ce202d64b7ac8047c0f584
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-11-03 22:29:12 +0100

    feat(api): add cookie parser dependency

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 import appointment from './routes/appointmentRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const swaggerDocument = YAML.load('./openapi.yaml');
 
@@ -12,6 +13,7 @@ export default function () {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
 
   app.get('/', (req, res) => {
     res.send('API is running correctly');
