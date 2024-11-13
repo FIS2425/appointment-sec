19d8a559c4b649ddc4976e90fbca4b51acb0f10b
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-11-13 17:51:33 +0100

    feat: added insert many materialized views

--- src/config/workshiftSync.js
+++ src/config/workshiftSync.js
@@ -15,6 +15,9 @@ export async function processWorkshiftMessage(msg) {
   case 'workshift-deleted':
     await deleteWorkshiftView(obj.workshift);
     break;
+  case 'workshifts-many':
+    await createManyWorkshifts(obj.workshifts);
+    break;
   default:
     console.error('Unknown event type:', obj.event);
   }
@@ -32,6 +35,11 @@ export async function updateWorkshiftView(workshiftData) {
 export async function deleteWorkshiftView(workshiftData) {
   await Workshift.findByIdAndDelete(workshiftData._id);
 }
+
+export async function createManyWorkshifts(workshifts) {
+  await Workshift.insertMany(workshifts);
+}
+
 
 export async function syncWorkshifts(workshifts) {
   await Workshift.deleteMany({});
