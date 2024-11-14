310d5df8b75c64d56f333a69034d7b424d6c91f8
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-11-14 10:04:56 +0100

    feat: include server footer

--- src/config/rabbitmq.js
+++ src/config/rabbitmq.js
@@ -14,21 +14,21 @@ async function connectRabbitMQ() {
       // Close connection when the Node.js process ends
       process.on('exit', () => {
         connection.close();
-        console.log('RabbitMQ connection closed');
+        console.log('[RabbitMQ] Connection closed');
       });
 
       // Handle connection errors
       connection.on('error', (err) => {
-        console.error('RabbitMQ connection error:', err);
+        console.error('[RabbitMQ] Connection error:', err);
         channel = null; // Reset channel so it will be recreated on the next request
       });
 
       connection.on('close', () => {
-        console.log('RabbitMQ connection closed');
+        console.log('[RabbitMQ] Connection closed');
         channel = null;
       });
     } catch (error) {
-      console.error('Failed to connect to RabbitMQ', error);
+      console.error('[RabbitMQ] Failed to connect to RabbitMQ', error);
       throw error;
     }
   }
@@ -46,7 +46,7 @@ async function startConsumer() {
 
     // Bind the queue to the exchange
     await channel.bindQueue(queueName, exchangeName, '');
-    console.log(`Waiting for messages in queue: ${queueName}`);
+    console.log(`[RabbitMQ] Waiting for messages in queue: ${queueName}`);
 
     channel.consume(queueName, (msg) => {
       if (msg !== null) {
